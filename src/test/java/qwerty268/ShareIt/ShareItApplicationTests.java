c37b4d1cda730673d2049fc2e4c42f0a87d95948
qwerty_268 <lev-demchenko@bk.ru>
2022-07-07 15:54:29 +0300

    ShareIt created

---
+++ src/test/java/qwerty268/ShareIt/ShareItApplicationTests.java
@@ -0,0 +1,13 @@
+package qwerty268.ShareIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
